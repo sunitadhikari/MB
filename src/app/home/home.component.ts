194614ddf14dc5e072b3f42f49ad0238584bd13d
sunitadhikari <sunitaadhikari2001@gmail.com>
2024-01-05 22:56:07 +0545

    transfer design

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -35,6 +35,6 @@ export class HomeComponent implements OnInit {
     this.route.navigate(['login']);
   }
   ngOnInit(): void {
-    this.selectSection = 'transfer';
+    this.selectSection = 'transaction';
   }
 }
--- src/app/transfer/transfer.component.ts
+++ src/app/transfer/transfer.component.ts
@@ -1,12 +1,11 @@
+import { MatIconModule } from '@angular/material/icon';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-transfer',
   standalone: true,
-  imports: [],
+  imports: [MatIconModule],
   templateUrl: './transfer.component.html',
-  styleUrl: './transfer.component.css'
+  styleUrl: './transfer.component.css',
 })
-export class TransferComponent {
-
-}
+export class TransferComponent {}
